780aa3f237da8c016f949d2de50773c239f8a94c
Aarize <heyman955@gmail.com>
2023-02-02 22:23:57 +0530

    Added Data

--- Leadzen/src/App.jsx
+++ Leadzen/src/App.jsx
@@ -1,18 +1,92 @@
-import { useState } from 'react'
+import { useState ,useEffect} from 'react'
 import Card from './components/Card'
 import Navbar from './components/Navbar'
 import './App.css'
+import axios from 'axios'
+import { createContext } from 'react'
+export const ThemeContext = createContext(null)
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [data, setData] = useState([])
+  const [page, setPage] = useState(1)
+  
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(res => {
+        setData(res.data)
+        
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+  
+  const selectPageHandler=(selectedPage)=>{
+    if(selectedPage>=1 && selectedPage <=Math.ceil(data.length/3) && selectedPage!==page)
+    setPage(selectedPage)
+  }
+ 
 
   return (
-    <div className="App">
-      <Navbar/>
-     <Card/>
+
+    
+    <div className="App" >
+      <Navbar />
+      <div className='mt-[6rem]' >
+      {data.slice(page*3-3,page*3).map((item) => {
+        return (
+          <Card
+            key={item.id}
+            name={item.name}
+            username={item.username}
+            email={item.email}
+            phone={item.phone}
+            company={item.company.name}
+            website={item.website}
+            catchphrase={item.company.catchPhrase}
+            bs={item.company.bs}
+            address={item.address.street+ " "+ item.address.suite+" "+item.address.city+" "+item.address.zipcode+" "}
+            street={item.address.street}
+            suite={item.address.suite}
+            city={item.address.city}
+            zipcode={item.address.zipcode}
+            lat={item.address.geo.lat}
+            lng={item.address.geo.lng}
+          />
+        )
+      })
+      }
+      {
+        data.length>0 &&(
+          <div className='pagination'>
+            
+            <span onClick={()=>selectPageHandler(page-1)}
+             className={page>1?'':'hidden'}
+            >◀</span>
+            {
+             [...Array(Math.ceil(data.length/3))].map((_,index)=>{
+                return(
+                  <span 
+                  className={page===index+1?'bg-[#ef4444] text-white':''}
+                  key={index} onClick={()=>selectPageHandler(index+1)}>{index+1}</span>
+                )
+              })
+            }
+            <span onClick={()=>selectPageHandler(page+1)}
+            className={page<Math.ceil(data.length/3)?'':'hidden'}
+            >▶</span>
+          </div>
+        )
+      }
+      </div>
+
     
     </div>
+
   )
 }
+
 
 export default App
--- Leadzen/src/components/Card.jsx
+++ Leadzen/src/components/Card.jsx
@@ -1,101 +1,104 @@
 import React,{useState} from 'react'
 import './Popup.css'
 import Popup from './popup'
-const Card = () => {
+const Card = (props) => {
   const [buttonPopup, setButtonPopup] = useState(false)
  
 const view=()=>{
   setButtonPopup(true);
 }
   return (
-    <div className='mt-[6rem]'>
+   
+    <div className='mt-[1rem]'>
     <div className='border-solid border-2 border-grey rounded-xl mt-[10px] bg-white shadow-lg shadow-[grey]'>
       <div className='flex  justify-between items-center m-10 '>
         <div>
-          <h1 className='text-[15px]  text-black'>UserName</h1>
+          <h1 className='text-[15px]  text-black'>{props.username}</h1>
         </div>
         <div>
           <h1 className='text-xl flex flex-col font-bold text-black'>Company</h1>
-          <p className='text-[grey]' >details</p>
+          <p className='text-[grey]' >{props.company}</p>
         </div>
         <div>
           <h1 className='text-xl flex flex-col font-bold text-black'>Website</h1>
-          <p className='text-[grey]'>details</p>
+          <p className='text-[grey]'>{props.website}</p>
         </div>
         <div>
           <h1 className='text-xl flex flex-col font-bold text-black'>City</h1>
-          <p className='text-[grey]'>details</p>
+          <p className='text-[grey]'>{props.city}</p>
         </div>
         <div>
           <button className='bg-[#ef4444] hover:bg-[#dc2626] text-white font-bold py-2 px-4 rounded-full' onClick={view}>View Details</button>
         </div>
       </div>
     </div>
+
     <Popup trigger={buttonPopup} setTrigger={setButtonPopup} >
-    <div className="h-[100%] flex justify-between rounded-xl items-center border-solid border-grey border-[2px] ">
-       <div>
-        <div className='flex items-center'>
+    <div className="h-[100%] grid rounded-xl items-center border-solid border-grey border-[2px] ">
+    <p className='text-[15px] font-bold text-[gray]'>{props.catchphrase}</p>
+        <p className='text-[15px] font-bold text-[grey]'>{props.bs}</p>
+       <div className='ml-5'>
+        <div className='flex items-center '  >
         <h1 className='text-[15px] mr-5 font-bold'>Name</h1>
-         <p className='text-[black] text-[12px] '>name</p>
+         <p className='text-[black] text-[12px] '>{props.name}</p>
         </div>
         <div className='flex items-center'>
         <h1 className='text-[15px] mr-5 font-bold'>Username</h1>
-        <p className='text-[black] text-[12px] '>username</p>
+        <p className='text-[black] text-[12px] '>{props.username}</p>
         </div>
         <div className='flex items-center'>
         <h1 className='text-[15px] mr-5 font-bold'>Email</h1>
-        <p className='text-[black] text-[12px] '>email</p>
+        <p className='text-[black] text-[12px] '>{props.email}</p>
         </div>
         <div className='flex items-center'>
         <h1 className='text-[15px] mr-5 font-bold'>Contact</h1>
-        <p className='text-[black] text-[12px] '>phone</p>
+        <p className='text-[black] text-[12px] '>{props.phone}</p>
         </div>
         </div>
-        <div>
+        <div className='ml-5'>
           <div className='flex items-center'>
         <h1 className='text-[15px] font-bold mr-5'>Company</h1>
-        <p className='text-[black] text-[12px] '>name</p>
+        <p className='text-[black] text-[12px] '>{props.company}</p>
           </div>
           <div className='flex items-center'>
         <h1 className='text-[15px] font-bold mr-5'>Website</h1>
-        <p className='text-[black] text-[12px] '>website</p>
+        <p className='text-[black] text-[12px] '>{props.website}</p>
           </div>
-        <p className='text-[15px] font-bold'>Catch-Phrase</p>
-        <p className='text-[15px] font-bold'>bs</p>
+        
         </div>
-        <div>
+        <div className='ml-5'>
           <div className='flex items-center'>
           <h1 className='text-[15px] font-bold mr-5'>Address</h1>
-          <p className='text-[black] text-[12px] '>Address</p>
+          <p className='text-[black] text-[12px] '>{props.address}</p>
           </div>
-          <div className='flex  justify-between border-[grey] border-solid border-[2px]'> 
+          <div className='flex  justify-between '> 
           <div>
-          <div>
-          <h1 className='text-[15px] font-bold'>street</h1>
-          <p className='text-[black] text-[12px] '>street</p>
+          <div className='flex items-center'>
+          <h1 className='text-[15px] font-bold mr-5'>street</h1>
+          <p className='text-[black] text-[12px] '>{props.street}</p>
           </div>
-          <div>
-          <h1 className='text-[15px] font-bold'>suite</h1>
-          <p className='text-[black] text-[12px] '>suite</p>
+          <div className='flex items-center'>
+          <h1 className='text-[15px] font-bold mr-5'>suite</h1>
+          <p className='text-[black] text-[12px] '>{props.suite}</p>
           </div>       
-          <div>
-          <h1 className='text-[15px] font-bold'>city</h1>
-          <p className='text-[black] text-[12px] '>city</p>
+          <div className='flex items-center'>
+          <h1 className='text-[15px] font-bold mr-5'>city</h1>
+          <p className='text-[black] text-[12px] '>{props.city}</p>
           </div>
           </div>
-          <div>
+          <div className='mr-[50%]'>
                  
-          <div>
-          <h1 className='text-[15px] font-bold'>zipcode</h1>
-          <p className='text-[black] text-[12px] '>zipcode</p>
+          <div className='flex items-center'>
+          <h1 className='text-[15px] font-bold mr-5'>zipcode</h1>
+          <p className='text-[black] text-[12px] '>{props.zipcode}</p>
           </div>
-          <div>
-          <h1 className='text-[15px] font-bold'>lat</h1>
-          <p className='text-[black] text-[12px] '>lat</p>  
+          <div className='flex items-center'>
+          <h1 className='text-[15px] font-bold mr-5'>lat</h1>
+          <p className='text-[black] text-[12px] '>{props.lat}</p>  
           </div>      
-          <div>
-          <h1 className='text-[15px] font-bold'>lng</h1>
-          <p className='text-[black] text-[12px] '>lng</p> 
+          <div className='flex items-center'>
+          <h1 className='text-[15px] font-bold mr-5'>lng</h1>
+          <p className='text-[black] text-[12px] '>{props.lng}</p> 
          </div> 
          </div> 
          </div>   
